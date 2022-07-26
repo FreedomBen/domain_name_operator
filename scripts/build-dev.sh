7fb4aa6ddfaec565387555e945c8bb6688abb7c6
Benjamin Porter <FreedomBen@users.noreply.github.com>
2022-07-26 17:47:11 -0600

    Fix dev image name

--- scripts/build-dev.sh
+++ scripts/build-dev.sh
@@ -3,7 +3,7 @@
 LATEST_VERSION='20220401110701'
 
 docker build \
-  -f Dockerfile \
+  -f Dockerfile.dev \
   -t "docker.io/freedomben/domain-name-operator-dev:${LATEST_VERSION}" \
   -t "docker.io/freedomben/domain-name-operator-dev:latest" \
   .
